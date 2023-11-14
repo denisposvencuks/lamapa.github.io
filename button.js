86167f171d0bcc316ddfdb61ac3e1d7bd7024e3c
denisposvencuks <81385519+denisposvencuks@users.noreply.github.com>
2023-11-14 23:28:48 +0200

    Update button.js

--- button.js
+++ button.js
@@ -2,22 +2,22 @@
 		Lampa.SettingsApi.addParam({
 			component: 'Multi_Menu_Component',
 			param: {
-				name: 'YouTube',
+				name: 'vlc',
 				type: 'trigger',
 				//доступно select,input,trigger,title,static
 				default: false
 			},
 			field: {
-				name: 'Раздел YouTube',
+				name: 'Раздел vlc',
 				//Название подпункта меню
-				description: 'Добавляет YouTube в главном меню' //Комментарий к подпункту
+				description: 'Добавляет vlc в главном меню' //Комментарий к подпункту
 			},
 			onChange: function(value) {
 				//Действия при изменении подпункта
-				if(Lampa.Storage.field('YouTube') == false) {
+				if(Lampa.Storage.field('vlc') == false) {
 					$('#YouTubeButton').addClass('hide');
 				}
-				if(Lampa.Storage.field('YouTube') == true) {
+				if(Lampa.Storage.field('vlc') == true) {
 					$('#YouTubeButton').removeClass('hide');
 				}
 				//Lampa.Settings.update();
